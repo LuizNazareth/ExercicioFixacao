a0bdca27a039679a22f65a2130fb2342f6e59cbf
Luiz Gustavo <luiz.nazareth@estudante.ufjf.br>
2023-01-27 13:02:49 -0300

    Exercicio Resolvido

---
+++ ExercicioFixacaoCap6/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacaoCap6
+{
+    class Funcionario
+    {
+        public string Id { get; private set; }
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+        public Funcionario(string id, string nome, double salario) 
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Salario = salario;
+        }
+
+        public void aumentoSalarial(double x)
+        {
+            this.Salario += this.Salario * x/100;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + this.Id + ", nome: " + this.Nome + ", salario" + this.Salario;
+        }
+    }
+}
---
+++ ExercicioFixacaoCap6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFixacaoCap6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos Funcionarios?");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Funcionario> list = new List<Funcionario>();
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Digite o id, nome e salario do funcionario: ");
+                string ident = Console.ReadLine();
+                string nome = Console.ReadLine();
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                i++;
+
+                Funcionario f = new Funcionario(ident, nome, salario);
+
+                list.Add(f);
+
+            } while (i < n);
+
+            Console.WriteLine("Qual o Id e o valor de aumento do funcionário?");
+
+            string id = Console.ReadLine();
+            double valor = double.Parse(Console.ReadLine());
+
+            int j = 0;
+            int k = 0;
+
+            foreach(Funcionario f in list)
+            {
+                if(f.Id == id)
+                {
+                    int index = j;
+                    k++;
+                    list[index].aumentoSalarial(valor);
+                }
+                j++;
+            }
+            if(k == 0)
+            {
+                Console.WriteLine("Id não encontrado");
+            }
+            else
+            {
+                Console.WriteLine("Listagem atualizada dos funcionarios: ");
+                foreach(Funcionario f in list)
+                {
+                    Console.WriteLine(f.ToString());
+                }
+            }
+        }
+    }
+}
